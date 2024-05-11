f0fc54fdba779b50fb289a431af8f853ae33cea3
Denis Mironov <denis5918@gmail.com>
2024-05-11 20:18:48 +0700

    fix

--- app/src/main/java/com/mironov/mironovtesttask/data/mapper/Mappers.kt
+++ app/src/main/java/com/mironov/mironovtesttask/data/mapper/Mappers.kt
@@ -4,9 +4,10 @@ import com.mironov.mironovtesttask.data.network.model.PokemonItemDto
 import com.mironov.mironovtesttask.domain.entity.PokemonItem
 
 private const val DEFAULT_IMAGE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+private val regex = Regex("mon/(\\d+)/")
 
 fun PokemonItemDto.toEntity(): PokemonItem {
-    val id = url.dropLast(1).last().digitToInt()
+    val id = regex.find(url)?.groupValues?.get(1) ?: "1"
     val iconUrl = "$DEFAULT_IMAGE_URL$id.png"
     return PokemonItem(
         name = name,
