1c8c9bb74da01a2623dab472defec860c982bba8
Denis Mironov <denis5918@gmail.com>
2024-05-11 16:20:44 +0700

    реализовал presentation layer

---
+++ app/src/main/java/com/mironov/mironovtesttask/presentation/pokemon_list/PokemonsEvent.kt
@@ -0,0 +1,20 @@
+package com.mironov.mironovtesttask.presentation.pokemon_list
+
+import com.mironov.mironovtesttask.domain.entity.PokemonItem
+
+sealed interface PokemonsEvent {
+
+    sealed class Ui : PokemonsEvent {
+
+        data object Init : Ui()
+
+        data object Refresh : Ui()
+    }
+
+    sealed class Domain : PokemonsEvent {
+
+        data class LoadingSuccess(val pokemonList: List<PokemonItem>) : Domain()
+
+        data object LoadingFailure : Domain()
+    }
+}
