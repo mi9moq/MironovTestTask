82c214c77104ad01853ae8b18f5ddaf8cd8770c0
Denis Mironov <denis5918@gmail.com>
2024-05-12 14:04:17 +0700

    fix

--- app/src/main/java/com/mironov/mironovtesttask/data/network/model/PokemonDetailDto.kt
+++ app/src/main/java/com/mironov/mironovtesttask/data/network/model/PokemonDetailDto.kt
@@ -1,5 +1,8 @@
 package com.mironov.mironovtesttask.data.network.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class PokemonDetailDto(
     val id: Int,
     val name: String,
